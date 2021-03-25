fd3e7a5a6500ed7c2b4fd060b92ffd03345dc9d9
Eduardo Saavedra <eduardo.saavedra@pluto.tv>
2021-03-25 16:37:57 -0500

    subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -11,21 +11,27 @@ export default {
         users.push(user);
         return user;
     },
-    createPost: (parent, { data }, { db: { posts } }, info) => {
+    createPost: (parent, { data }, { pubSub, db: { posts, users } }, info) => {
         const userExists = users.some(({ id }) => id === data.author);
         if (!userExists) { throw new Error('no user.') };
         const post = { id: v4(), ...data }
         posts.push(post);
+        if(data.published) {
+            pubSub.publish('post',  {post: {
+                mutation: 'CREATED',
+                data: post
+            }})
+        };
         return post;
     },
-    createComment: (parent, { data }, { pubSub, db: { comments } }, info) => {
+    createComment: (parent, { data }, { pubSub, db: { comments, users, posts } }, info) => {
         const userExists = users.some(({ id }) => id === data.author);
         const published = posts.find(({ id, published }) => id === data.post && published);
         if (!userExists) { throw new Error('No User.') }
         else if (!published) { throw new Error('Not published') }
         const comment = { id: v4(), ...data }
         comments.push(comment);
-        pubSub.publish(`comment ${id}`)
+        pubSub.publish(`comment ${data.post}`, {comment: {mutation: 'CREATED', data: comment}});
         return comment;
     },
     /*
@@ -44,19 +50,24 @@ export default {
         comments = comments.filter(({ author }) => author !== id);
         return deleted[ 0 ];
     },
-    deletePost: (parent, { id }, { db: { posts } }, info) => {
+    deletePost: (parent, { id }, { pubSub, db: { posts, comments } }, info) => {
         id = parseInt(id);
         const postIndex = posts.findIndex((post) => post.id === id);
         if (postIndex < 0) throw new Error('no Post');
-        const deleted = posts.splice(postIndex, 1);
+        const [post] = posts.splice(postIndex, 1);
         comments = comments.filter((comment) => comment.post !== id);
-        return deleted[ 0 ];
+        if (post.published) {
+            pubSub.publish('post', {post: { mutation: 'DELETED', data: post}})
+        }
+        return post;
     },
-    deleteComment: (parent, { id }, { db: { comments } }, info) => {
+    deleteComment: (parent, { id }, { pubSub, db: { comments } }, info) => {
         id = parseInt(id);
         const commentIndex = comments.findIndex((comment) => comment.id === id);
         if (commentIndex < 0) throw new Error('no Comment');
-        return comments.splice(commentIndex, 1)[ 0 ];
+        const [comment] = comments.splice(commentIndex, 1);
+        pubSub.publish(`comment ${comment.post}`, {comment: { mutation: 'DELETED', data: comment}});
+        return comment
     },
 
     /*
@@ -77,24 +88,29 @@ export default {
 
         return user;
     },
-    updatePost: (parent, { id, data: { title, body, published } }, { db: { posts } }, info) => {
+    updatePost: (parent, { id, data: { title, body, published } }, { pubSub, db: { posts } }, info) => {
         id = parseInt(id);
         const post = posts.find((post) => post.id === id);
+        const oPost = {...post};
         if (!post) throw new Error('no post');
 
         if (title) post.title = title;
         if (body) post.name = body;
-        if (typeof(published) === Boolean) post.published = published;
+        if (typeof(published) === Boolean) {
+            post.published = published
+            if (oPost.published && !post.published) {pubSub.publish('post', {post: { mutation: 'DELETED', data: oPost}})}
+            else if (!oPost.published && post.published) {pubSub.publish('post', {post: { mutation: 'CREATED', data: post}})}
+        } else if (post.published) { pubSub.publish('post', {post: { mutation: 'UPDATED', data: post}}) }
 
         return post;
     },
-    updateComment: (parent, { id, data: { body } }, { db: { comments } }, info) => {
+    updateComment: (parent, { id, data: { body } }, { pubSub, db: { comments } }, info) => {
         id = parseInt(id);
         const comment = comments.find((comment) => comment.id === id);
         if (!comment) throw new Error('no comment');
 
         if (body) comment.body = body;
-
+        pubSub.publish(`comment ${comment.post}`, {comment: { mutation: 'UPDATED', data: comment}})
         return comment;
     }
 }
--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,20 +1,15 @@
 export default {
-    count: {
-        subscribe: (parent, args, { pubSub }, info) => {
-            let count = 0;
-            setInterval(() => {
-                count++;
-                pubSub.publish('count', { count });
-            }, 1000)
-            return pubSub.asyncIterator('count');
+    comment: {
+        subscribe: (parent, { postId }, { pubSub, db: { posts } }, info) => {
+            const post = posts.find(({ id, published }) => parseInt(postId) === parseInt(id) && published);
+            if(!post) throw new Error('no post');
+            return pubSub.asyncIterator(`comment ${postId}`);
         }
     },
 
-    comment: {
-        subscribe: (parent, { postId }, { pubSub, db: { posts } }, info) => {
-            const post = posts.find(({ id, published }) => postId === id && published);
-            if(!post) throw new Error('no post');
-            pubSub.asyncIterator(`comment ${postId}`);
+    post: {
+        subscribe: (parent, {}, {pubSub}, info) => {
+            return pubSub.asyncIterator(`post`);
         }
     }
 }
